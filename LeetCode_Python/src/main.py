fbe5d0957db5f7b6671610789eeffe285d936ee5
ArMonarch <praffulthapa12@gmail.com>
2025-11-26 19:26:33 +0545

    feat: added LeetCode Python directly insted of submodules.

---
+++ LeetCode_Python/src/main.py
@@ -0,0 +1,74 @@
+# LeetCode Problem - 88. [Merge Sorted Array](https://leetcode.com/problems/merge-sorted-array)
+import merge_sorted_array
+_ = merge_sorted_array
+
+# LeetCode Problem - 27. [Remove Element](https://leetcode.com/problems/remove-element)
+import remove_element
+_ = remove_element
+
+# Leetcode Problem - 26. [Remove Duplicate from Sorted Array](https://leetcode.com/problems/remove-duplicates-from-sorted-array)
+import remove_duplicates
+_ = remove_duplicates
+
+# Leetcode Problem - 169. [Majority Element](https://leetcode.com/problems/majority-element)
+import majority_element
+_ = majority_element
+
+# Leetcode Problem - 121. [Best Time to Buy and Sell Stocks](https://leetcode.com/problems/best-time-to-buy-and-sell-stock)
+import best_time_to_buy_and_sell_stocks
+_ = best_time_to_buy_and_sell_stocks
+
+# Leetcode Problem - 13. [Roman to Number](https://leetcode.com/problems/roman-to-integer)
+import roman_to_integer
+_ = roman_to_integer
+
+# Leetcode Problems - 58 [Length of The Last Word](https://leetcode.com/problems/length-of-last-word)
+import length_of_last_word
+_ = length_of_last_word
+
+# Leetcode Problems - 14 [Longest Common Prefix](https://leetcode.com/problems/longest-common-prefix)
+import longest_common_prefix
+_  = longest_common_prefix
+
+# Leetcode Problems - 28. [Find the Index of the First Occurrence in a String](https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string)
+import find_first_occurance
+_ = find_first_occurance
+
+# LeetCode Problem - [Contains Duplicate](https://leetcode.com/problems/contains-duplicate)
+import contains_duplicate
+_ = contains_duplicate
+
+# LeetCode Problem - [Missing Numbers](https://leetcode.com/problems/missing-number)
+import missing_numbers
+_ = missing_numbers
+
+# LeetCode Problem - [Find All Numbers Disappeared in an Array](https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array)
+import find_disappeared_numbers
+_ = find_disappeared_numbers
+
+# LeetCode Problem - [Two Sum](https://leetcode.com/problems/two-sum)
+import two_sum
+_ = two_sum
+
+# LeetCode Problem - [Final Value After Performing Operations](https://leetcode.com/problems/final-value-of-variable-after-performing-operations)
+import final_value_after_operation
+_ = final_value_after_operation
+
+# LeetCode Problem - [How Many Numbers Are Smaller Than The Current Number](https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number)
+import smaller_number_than_current
+_ = smaller_number_than_current
+
+# LeetCode Problem - [Minimum Time Visiting All Points](https://leetcode.com/problems/minimum-time-visiting-all-points)
+import minimum_time_visit_points
+_ = minimum_time_visit_points
+
+# LeetCode Problem - [Spiral Martix](https://leetcode.com/problems/spiral-matrix)
+import spiral_matrix
+_ = spiral_matrix
+
+# LeetCode Problem - [Number of Islands](https://leetcode.com/problems/number-of-islands)
+import number_of_islands
+_ = number_of_islands
+
+if __name__ == "__main__":
+   print("Leetcode Problem Solution!")
