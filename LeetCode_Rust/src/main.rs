e8b788f51d53c68973cd770ff0ab9f09fd2806e1
ArMonarch <praffulthapa12@gmail.com>
2025-11-26 19:26:40 +0545

    feat: added LeetCode Rust directly insted of submodules.

---
+++ LeetCode_Rust/src/main.rs
@@ -0,0 +1,82 @@
+// LeetCode Problem 1. [Two Sum](https://leetcode.com/problems/two-sum)
+mod two_sum;
+
+// LeetCode Problem - 412. [Fizz Buzz](https://leetcode.com/problems/fizz-buzz)
+mod fizz_buzz;
+
+// LeetCode Problem - 344. [Reverse String](https://leetcode.com/problems/reverse-string)
+mod reverse_string;
+
+// LeetCode Problem - 69. [Sqrt(x)](https://leetcode.com/problems/sqrtx)
+mod sqrt;
+
+// LeetCode Problem - 268. [Missing Number](https://leetcode.com/problems/missing-number)
+mod missing_number;
+
+// LeetCode Problem - 326. [Power of Three](https://leetcode.com/problems/power-of-three)
+mod power_of_three;
+
+//LeetCode Problem - 66. [Plus One](https://leetcode.com/problems/plus-one)
+mod plus_one;
+
+// LeetCode Problem - 206. [Reverse Linked List](https://leetcode.com/problems/reverse-linked-list)
+mod reverse_linked_list;
+
+//  LeetCode Problem - 94. [Binary Tree Inorder Taversal](https://leetcode.com/problems/binary-tree-inorder-traversal)
+mod binary_tree_inorder_traversal;
+
+// LeetCode Problem - 104. [Maximum Depth of Binary Tree](https://leetcode.com/problems/maximum-depth-of-binary-tree)
+mod max_depth_binary_tree;
+
+// LeetCode Problem - 118. [Pascal's Traingle](https://leetcode.com/problems/pascals-triangle)
+mod pascal_traingle;
+
+// LeetCode Problem - 136. [Singel Number](https://leetcode.com/problems/single-number)
+mod single_number;
+
+// LeetCode Problem - 191. [Nuumber of 1 Bits](https://leetcode.com/problems/number-of-1-bits)
+mod number_of_1bits;
+
+// LeetCode Problem - 20. [Valid Parentheses](https://leetcode.com/problems/valid-parentheses)
+mod valid_parentheses;
+
+// LeetCode Problem - 21. [Merge Two Sorted Lists](https://leetcode.com/problems/merge-two-sorted-lists)
+// mem::swap() method, that takes O(1) space complexity & O(N) time complexity
+mod merge_two_sorted;
+
+// LeetCode Problem - 70. [Climbing Stairs](https://leetcode.com/problems/climbing-stairs)
+mod climbing_stairs;
+
+// LeetCode Problem - 101. [Symmetric Tree](https://leetcode.com/problems/symmetric-tree)
+mod symmetric_tree;
+
+// LeetCode Problem - 88. [Merge Sorted Array](https://leetcode.com/problems/merge-sorted-array)
+mod merge_sorted_array;
+
+// LeetCode Problem - 27. [Remove Element](https://leetcode.com/problems/remove-element)
+mod remove_element;
+
+// LeetCode Problem - 26. [Remove duplicates](https://leetcode.com/problems/remove-duplicates-from-sorted-array)
+mod remove_duplicates;
+
+// LeetCode Problem - 169. [Majority Element](https://leetcode.com/problems/majority-element)
+mod majority_element;
+
+// LeetCode Problem - 121. [Best Time to Buy and Sell Stock](https://leetcode.com/problems/best-time-to-buy-and-sell-stock)
+mod best_buy_sell_stock;
+
+// LeetCode Problem - 13. [Roman to Interger](https://leetcode.com/problems/roman-to-integer)
+mod roman_to_number;
+
+// LeetCode Problem - 121. [Length of Last Word](https://leetcode.com/problems/length-of-last-word)
+mod length_of_last_word;
+
+// LeetCode Problem - 14. [Longest Common Prefix](https://leetcode.com/problems/longest-common-prefix)
+mod longest_common_prefix;
+
+// LeetCode Problems - 28. [Find the Index of the First Occurrence in a String](https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string)
+mod find_first_occurance;
+
+fn main() {
+    println!("LeetCode Problems Solution!");
+}
